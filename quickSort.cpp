83c7664ad6ceb3e5bf3dd62a6ec3495fc2a25570
Shiraj Ganguly <shirajganguly@gmail.com>
2016-10-22 18:23:08 -0400

    Add partitioning logic

--- quickSort.cpp
+++ quickSort.cpp
@@ -5,26 +5,50 @@
 
 using namespace std;
 
-int choosePivot(int start, int end) {
-  int pivot = start + (abs(end - start) / 2);
-  cout << " Start: " << start << " End: " << end << " Pivot: " << pivot  << endl;
-  return pivot;
+vector<int> numbers = {9, 8, 11, 13, 3, 10, 4, 5, 12, 6, 7};
+
+void printArray(vector<int> arr) {
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << ", ";
+  }
+  cout << endl;
 }
 
-void partition(vector<int>* numbers, int start, int end, int pivot) {
-  cout << "Partitioning" << endl;
+void partition(int start, int end) {
+  int pivotVal = numbers[start];
+  int seen = 0;
+  int border = 0;
+  int temp;
+  cout << "Pivot is: " << pivotVal << endl;
+
+  for (int i = start + 1; i <= end; i++) {
+    cout << numbers[i] << endl;
+    if (numbers[i] < pivotVal) {
+      temp = numbers[i];
+      numbers[i] = numbers[border + 1];
+      numbers[border + 1] = temp;
+      border++;
+    }
+  }
+
+  temp = numbers[border];
+  numbers[border] = pivotVal;
+  numbers[start] = temp;
+
+  printArray(numbers);
 }
 
-void quickSort(vector<int>* numbers, int start, int end) {
+void quickSort(int start, int end) {
+  cout << numbers.size() << endl;
   if (start >= end) return;
-  int pivot = choosePivot(start, end);
-  partition(numbers, start, end, pivot);
-  quickSort(numbers, start, pivot - 1);
-  quickSort(numbers, pivot + 1, end);
+  // int pivotPosition = end;
+  // int pivotValue = (*numbers)[pivotPosition];
+  partition(start, end);
+  // quickSort(numbers, start, pivotPostion - 1);
+  // quickSort(numbers, pivotPosition + 1, end);
 }
 
 int main() {
-  vector<int> numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-  quickSort(&numbers, 0, numbers.size());
+  quickSort(0, numbers.size() - 1);
   return 0;
 }
